24bcf3dc68f32bb51946af8e3628ecf550043417
Ilya <victorieuse48@gmail.com>
2018-11-28 00:24:41 +0300

    Print method has been implemented.

--- ContactList.py
+++ ContactList.py
@@ -18,17 +18,26 @@ class ContactList:
         self.__json_file_name = json_name
         self.__initialize_list()
         self.__contacts_number = self.__contact_list.__len__()
+        if self.__contacts_number == 0:
+            print("\nInitial contact list is empty.")
+        else:
+            print("\nSuccessfully upload stored contact list. "
+                  "Number of contacts = " + str(self.__contacts_number))
 
     def __initialize_list(self):
-        with open(self.__json_file_name) as json_handler:
-            self.__contact_list = json.load(json_handler, object_hook=Contact.decode)
+        with open(self.__json_file_name, "r") as json_handler:
+            try:
+                self.__contact_list = json.load(json_handler, object_hook=Contact.decode)
+            except json.decoder.JSONDecodeError:
+                self.__contact_list = list()
 
-    def __push_to_data_base(self):
+    def __push_to_data_base(self, new_contact):
         with open(self.__json_file_name, "a") as json_handler:
-            json.dump(self.__contact_list, json_handler, default=Contact.encode, indent=4)
+            json.dump(new_contact, json_handler, default=Contact.encode, indent=4)
 
     def add_contact(self, name, surname, number, date=None):
         self.__contact_list.append(Contact(name, surname, number, date))
+        self.__push_to_data_base()
 
     def is_contact_in_list(self, name=None, surname=None, number=None, date=None):
         if name and surname and number and date:
@@ -72,3 +81,14 @@ class ContactList:
                 temp_list = contraction
 
             return temp_list
+
+    def print(self):
+        for item in self.__contact_list:
+            print(str(item.get_name()) + " " + str(item.get_surname()), end=' ')
+            if item.get_birth_date():
+                print(str(item.get_birth_date()))
+            else:
+                print()
+            for key, value in item.get_number_list().items():
+                print(key + " : " + str(value))
+            print()
