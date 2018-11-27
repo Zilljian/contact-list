69353e5cfe2852f9b4ccb4638257820d0401110e
Ilya <victorieuse48@gmail.com>
2018-11-27 17:29:06 +0300

    Static method to convert Contact to json serializable has been implemented. Auxiliary class' methods have been likewise implemented.

--- Contact.py
+++ Contact.py
@@ -1,14 +1,11 @@
 class Contact:
-    __name = ""
-    __surname = ""
-    __number = None
-    __birth_date = ""
 
     def __init__(self, name, surname, number, date=None):
         self.__name = name
         self.__surname = surname
-        self.__number = number
+        self.__number = {'Main': number}
         self.__birth_date = date
+        self.__number_list_len = 1
 
     def get_name(self):
         return self.__name
@@ -22,14 +19,30 @@ class Contact:
     def set_surname(self, new_surname):
         self.__surname = new_surname
 
-    def get_number(self):
+    def get_number(self, tag='Main'):
+        return self.__number[tag]
+
+    def set_number(self, number, tag='Main'):
+        self.__number[tag] = number
+        self.__number_list_len = self.__number.__len__()
+
+    def get_number_list(self):
         return self.__number
 
-    def set_number(self, number):
-        self.__number = number
+    def get_number_list_len(self):
+        return self.__number_list_len
 
     def get_birth_date(self):
         return self.__birth_date
 
     def set_birth_date(self, new_date):
         self.__birth_date = new_date
+
+    @staticmethod
+    def default(object):
+        if isinstance(object, Contact):
+            return {
+                'Name': object.get_name(),
+                'Surname': object.get_surname(),
+                'Number': object.get_number_list()}
+        raise TypeError
