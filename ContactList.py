0dd327cf7da00c7c5d7103524009dc55a8f92ab5
Ilya <victorieuse48@gmail.com>
2018-12-01 01:57:20 +0300

    remove_contact method has been implemented. Some bags has been fixed.

--- ContactList.py
+++ ContactList.py
@@ -1,6 +1,12 @@
 from Contact import Contact
 import json
 from ex import *
+import datetime
+
+
+"""
+Class containing all contacts and provide methods to manipulate them
+"""
 
 
 class ContactList:
@@ -16,7 +22,8 @@ class ContactList:
             print("\n=== Initial contact list is empty ===\n")
         else:
             print("\n=== Successfully upload stored contact list ===\n"
-                  "Number of contacts = " + str(self.__contacts_number))
+                  "    Number of contacts = " + str(self.__contacts_number))
+        self.__push_to_data_base()
         try:
             self.__init_name_list()
         except PairExistException as e:
@@ -29,14 +36,15 @@ class ContactList:
             except json.decoder.JSONDecodeError:
                 self.__contact_list = list()
 
-    def __push_to_data_base(self, new_contact):
-        with open(self.__json_file_name, "a") as json_handler:
-            json.dump(new_contact, json_handler, default=Contact.encode, indent=4)
+    def __push_to_data_base(self):
+        with open(self.__json_file_name, "w") as json_handler:
+            json.dump(self.__contact_list, json_handler, default=Contact.encode, indent=4)
 
     def add_contact(self, name, surname, number, date=None):
         self.__contact_list.append(Contact(name, surname, number, date))
         self.__contacts_number = self.__contact_list.__len__()
-        self.__push_to_data_base(self.__contact_list[-1])
+        self.__name_list.append((name, surname))
+        self.__push_to_data_base()
 
     def get_matched(self, name=None, surname=None, number=None, date=None):
         if name and surname and number and date:
@@ -62,22 +70,36 @@ class ContactList:
                 temp = temp_list if len(temp_list) != 0 else self.__contact_list
 
                 for item in temp:
-                    for num in item.get_number_list:
-                        if str(num) == number:
+                    temp2 = item.get_number_list()
+                    for value in temp2:
+                        if value == number:
                             contraction.append(item)
                             break
                 temp_list = contraction
 
             if date is not None:
+                date_sp = date.split('/')
+                temp_date = datetime.date(int(date_sp[2]), int(date_sp[1]), int(date_sp[0]))
                 contraction = list()
                 temp = temp_list if len(temp_list) != 0 else self.__contact_list
 
                 for item in temp:
-                    if item.get_date() == date:
+                    if item.get_birth_date() == temp_date:
                         contraction.append(item)
                 temp_list = contraction
 
             return temp_list
+
+    def remove_contact(self, name, surname):
+        for item in self.__contact_list:
+            if item.get_name() == name and item.get_surname() == surname:
+                self.__contact_list.remove(item)
+        self.__contacts_number -= 1
+
+        for item in self.__name_list:
+            if item[0] == name and item[1] == surname:
+                self.__name_list.remove(item)
+        self.__push_to_data_base()
 
     def print_all(self):
         for item in self.__contact_list:
